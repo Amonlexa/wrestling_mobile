94b2d7de9b688034c1f6ec1d852c3888b90d4652
Amonlexa <struchkov.01@list.ru>
2025-05-30 17:02:14 +0900

    fix: transfet env file from old project to nex

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/wrestling_hub_app.dart';
 import 'core/di.dart';
 
 void main() async {
+  await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDependencies();
   setWhiteStatusBar();
